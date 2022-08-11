6dc59230a2600d4a59fbd62b6ba86aaaff03103a
hanhqvu <hanhqvu@users.noreply.github.com>
2022-08-11 23:35:04 +0900

    feat: welcome message

--- routes/WelcomeScreen.js
+++ routes/WelcomeScreen.js
@@ -1,14 +1,30 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { Button } from "react-native-paper";
 
 export default function WelcomeScreen({ navigation }) {
 	return (
 		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-			<Text>Welcome</Text>
+			<View
+				style={{
+					paddingBottom: 15,
+					alignItems: "center",
+					justifyContent: "center",
+				}}
+			>
+				<Text style={styles.text}>Are you feeling hungry right now?</Text>
+				<Text style={styles.text}>Feeling confused? Don't know what to order?</Text>
+				<Text style={styles.text}>Don't worry, our app will help you!</Text>
+			</View>
 			<Button mode="contained" onPress={() => navigation.navigate("Main")}>
-				To Main
+				Let's get started
 			</Button>
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	text: {
+		fontSize: 18,
+	},
+});
